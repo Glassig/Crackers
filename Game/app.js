ff481613285308f53a24a9bceb1f8480bd4d2d52
Angelina <angelina.v.gegerfelt@gmail.com>
2016-03-22 14:31:54 +0100

    Made two pages, one for typing and one for speech
    If you open localhost:4000/type or /speech you will se them. Just for fun and to try it out.

--- Game/app.js
+++ Game/app.js
@@ -8,6 +8,14 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Saltineeeeees'});
 });
 
+app.get('/speech', function (req, res) {
+  res.render('speech', { title: 'Speech-version'});
+});
+
+app.get('/type', function (req, res) {
+  res.render('typing', { title: 'Typing-version'});
+});
+
 app.listen(4000, function () {
   console.log('Listening on port 4000!');
 });
