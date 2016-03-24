65aa03a4eb6cde3dfb08f03753e5716367d9b140
Angelina <angelina.v.gegerfelt@gmail.com>
2016-03-24 12:11:35 +0100

    Added a favicon and a readme for game, also deleted speech & typing file
    The speech and typing files were not usefull and only showed unproffesionalism (all boo Angelina).
    Stole a favicon online, so it should be edited at some point.
    Wrote a readme for the game, which now includes dependencies and instructions. should be expanded.

--- Game/app.js
+++ Game/app.js
@@ -1,20 +1,21 @@
 var express = require('express');
-//var hbs = require('express-hbs');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
+app.use(favicon('favicon.ico'));
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Saltiiiiiiiines'});
+  res.render('index', { title: 'Saltiiiiiiiines', txt: 'Welcome to our game! You rule!'});
 });
 
 app.get('/speech', function (req, res) {
-  res.render('speech', { title: 'Speech-version'});
+  res.render('index', { title: 'Speech-version', txt: 'This is the speech implementation! To speak is golden or something.'});
 });
 
 app.get('/typing', function (req, res) {
-  res.render('typing', { title: 'Typing-version'});
+  res.render('index', { title: 'Typing-version', txt: 'This is the typing version. HACKER MONTAGE SPEEED HOLLYWOOD BS!!!'});
 });
 
 app.listen(4000, function () {
