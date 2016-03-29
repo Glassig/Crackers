595df3aa7257f296cfca98951be119ca3bbfdef4
Angelina <angelina.v.gegerfelt@gmail.com>
2016-03-29 16:19:50 +0200

    synonyms in alfabethical order

--- Game/java/Synonyms.java
+++ Game/java/Synonyms.java
@@ -20,12 +20,12 @@ public class Synonyms {
 	//NOUNS maybe?
 
 
-/*
-key = String
-value = int: 
-	if 1: only 1 meaning
-	if 2: two possible meanins, look at noun. (for example, move to livingroom AND move the chair)
-*/
+	/*
+	key = String
+	value = int: 
+		if 1: only 1 meaning
+		if 2: two possible meanins, look at noun. (for example, move to livingroom AND move the chair)
+	*/
 	public Synonyms() {
 		createLists();
 
@@ -45,70 +45,71 @@ value = int:
 	}
 
 	private void addGo() {
-		go.put("go", 1);
-		go.put("walk", 1);
 		go.put("enter", 1);
+		go.put("go", 1);
 		go.put("move", 2);
+		go.put("walk", 1);
 	}
 
 	private void addTake() {
-		take.put("get", 1);
 		take.put("acquire", 1);
-		take.put("take", 1);
-		take.put("grab", 1);
-		take.put("pick", 1);
-		take.put("hold", 1);
-		take.put("grip", 1);
-		take.put("gather", 1);
 		take.put("attain", 1);
 		take.put("gain", 1);
-		take.put("snag", 1);
+		take.put("gather", 1);
+		take.put("get", 1);
+		take.put("grab", 1);
+		take.put("grip", 1);
+		take.put("hold", 1);
+		take.put("pick", 1);
 		take.put("seize", 1);
+		take.put("snag", 1);
+		take.put("take", 1);
 	}
 
 	private void addInteract() {
-		interact.put("use", 1);
-		interact.put("utilize", 1);
 		interact.put("interact", 1);
-		interact.put("throw", 1);
 		interact.put("push", 1);
 		interact.put("put", 1);
+		interact.put("throw", 1);
+		interact.put("use", 1);
+		interact.put("utilize", 1);
 	}
 
 	private void addLook() {
-		look.put("look", 1);
-		look.put("see", 1);
-		look.put("examine", 1);
-		look.put("discover", 1);
-		look.put("watch", 1);
-		look.put("glance", 1);
-		look.put("stare", 1);
-		look.put("study", 1);
-		look.put("view", 1);
-		look.put("peer", 1);
-		look.put("read", 1);
 		look.put("admire", 1);
 		look.put("behold", 1);
+		look.put("check", 1);
 		look.put("contemplate", 1);
-		look.put("survey", 1);
-		look.put("observe", 1);
-		look.put("scan", 1);
-		look.put("inspect", 1);
+		look.put("discover", 1);
+		look.put("examine", 1);
 		look.put("gaze", 1);
+		look.put("glance", 1);
+		look.put("inspect", 1);
+		look.put("look", 1);
+		look.put("observe", 1);
+		look.put("peer", 1);
+		look.put("read", 1);
+		look.put("scan", 1);
+		look.put("see", 1);
+		look.put("stare", 1);
+		look.put("study", 1);
+		look.put("survey", 1);
+		look.put("view", 1);
+		look.put("watch", 1);
 	}
 
 	private void addEat() {
-		eat.put("eat", 1);
-		eat.put("consume", 1);
-		eat.put("munch", 1);
+		eat.put("absorb", 1);
 		eat.put("bite", 1);
 		eat.put("chew", 1);
-		eat.put("devour", 1);
-		eat.put("feed", 1);
-		eat.put("absorb", 1);
-		eat.put("snack", 1);
 		eat.put("chow", 1);
+		eat.put("consume", 1);
+		eat.put("devour", 1);
+		eat.put("eat", 1);
+		eat.put("feed", 1);
 		eat.put("gobble", 1);
+		eat.put("munch", 1);
+		eat.put("snack", 1);
 	}
 
 	public ArrayList<String> verbTags(ArrayList<String> inputVerbs) {
