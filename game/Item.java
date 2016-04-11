8c67f335b5fb23f0501f6b436db55bd77737e694
Kashi <k.klingestedt@gmail.com>
2016-04-12 00:09:47 +0200

    edited project file

---
+++ game/Item.java
@@ -0,0 +1,119 @@
+package game;
+/*
+ * Item class
+ */
+
+import java.util.ArrayList;
+
+
+
+public class Item {
+
+    private String name;                    //e.g. key, ball, apple
+	private String type;                    //e.g. move, eat, go
+	private String descriptionTrue;         //when status is true
+    private String descriptionFalse;        //when status is false
+	private boolean status;                 //decides if it's accessable yet
+    private boolean viewable;               //if it's listed when you look around the room
+    private ArrayList<Item> dependables;    //items that depend on this item's status
+    private ArrayList<Item> usables;        //items you can use this item on
+    private ArrayList<Item> viewDependables;//items you can only discover by first looking at this item
+    
+
+	public Item(String name, String type, String descriptionTrue, String descriptionFalse, boolean status, boolean viewable){
+        this.name = name;
+    	this.type = type;
+    	this.descriptionTrue = descriptionTrue;
+        this.descriptionFalse = descriptionFalse;
+    	this.status = status;
+        this.viewable = viewable;
+        dependables = new ArrayList<Item>();
+        usables = new ArrayList<Item>();
+        viewDependables = new ArrayList<Item>();
+    }
+
+    /*
+    * Add a dependable item.
+    */
+    public void addDependable(Item item){
+        dependables.add(item);
+    }
+
+    /*
+    * Add a usable item.
+    */
+    public void addUsable(Item item){
+        usables.add(item);
+    }
+
+    /*
+    * Add a viewable dependable
+    */
+    public void addViewDependable(Item item){
+        viewDependables.add(item);
+    }
+
+
+
+    /*
+    * Getters
+    */
+    public String getName(){
+        return name;
+    }
+
+    public String getType(){
+    	return type;
+    }
+
+    public String getDescription(boolean status){
+        if(status){
+            return descriptionTrue;
+        }
+        return descriptionFalse;
+    }
+
+    public String getDescriptionTrue(){
+    	return descriptionTrue;
+    }
+
+    public String getDescriptionFalse(){
+        return descriptionFalse;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public boolean getViewable(){
+        return viewable;
+    }
+
+    public ArrayList<Item> getDependables(){
+        return dependables;
+    }
+
+    public ArrayList<Item> getUsables(){
+        return usables;
+    }
+
+    public ArrayList<Item> getViewDependables(){
+        return viewDependables;
+    }
+
+    /*
+    * Setters
+    */
+    public void setType(String newType){
+        type = newType;
+    }
+
+    public void setStatus(boolean newStatus){
+    	status = newStatus;
+    }
+
+    public void setViewable(boolean bool){
+        viewable = bool;
+    }
+
+} 
---
+++ game/Synonyms.java
@@ -0,0 +1,176 @@
+package game;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stanford.nlp.ling.TaggedWord;
+
+/*
+HashMap commands:
+	put(key, value) = add
+	get(key) = value
+	keySet() = set with keys
+	containsKey(key) = true if key is present
+*/
+public class Synonyms {
+
+	// VERBS
+	private HashMap<String, Integer> go;
+	private HashMap<String, Integer> take;
+	private HashMap<String, Integer> use;
+	private HashMap<String, Integer> look;
+	private HashMap<String, Integer> eat;
+
+	//NOUNS maybe?
+
+
+	/*
+	key = String
+	value = int: 
+		if 1: only 1 meaning
+		if 2: two possible meanins, look at noun. (for example, move to livingroom AND move the chair)
+	*/
+	public Synonyms() {
+		createLists();
+	}
+
+	private void createLists() {
+		go = new HashMap<String, Integer>();
+		take = new HashMap<String, Integer>();
+		eat = new HashMap<String, Integer>();
+		use = new HashMap<String, Integer>();
+		look = new HashMap<String, Integer>();
+		addGo();
+		addUse();
+		addTake();
+		addEat();
+		addLook();
+	}
+
+	private void addGo() {
+		go.put("enter", 1);
+		go.put("go", 1);
+		go.put("head", 1);
+		go.put("jump", 1);
+		go.put("move", 1);
+		go.put("run", 1);
+		go.put("stand", 1);
+		go.put("visit", 1);
+		go.put("walk", 1);
+	}
+
+	private void addTake() {
+		take.put("acquire", 1);
+		take.put("attain", 1);
+		take.put("gain", 1);
+		take.put("gather", 1);
+		take.put("get", 1);
+		take.put("grab", 1);
+		take.put("grip", 1);
+		take.put("hold", 1);
+		take.put("pick", 1);
+		take.put("seize", 1);
+		take.put("snag", 1);
+		take.put("steal", 1);
+		take.put("take", 1);
+	}
+
+	private void addEat() {
+		eat.put("absorb", 1);
+		eat.put("bite", 1);
+		eat.put("chew", 1);
+		eat.put("chow", 1);
+		eat.put("consume", 1);
+		eat.put("devour", 1);
+		eat.put("eat", 1);
+		eat.put("feed", 1);
+		eat.put("gobble", 1);
+		eat.put("ingest", 1);
+		eat.put("munch", 1);
+		eat.put("snack", 1);
+		eat.put("swallow", 1);
+	}
+
+	private void addLook() {
+		look.put("admire", 1);
+		look.put("behold", 1);
+		look.put("check", 1);
+		look.put("contemplate", 1);
+		look.put("discover", 1);
+		look.put("examine", 1);
+		look.put("gaze", 1);
+		look.put("glance", 1);
+		look.put("inspect", 1);
+		look.put("look", 1);
+		look.put("observe", 1);
+		look.put("peer", 1);
+		look.put("read", 1);
+		look.put("scan", 1);
+		look.put("search", 1);
+		look.put("see", 1);
+		look.put("stare", 1);
+		look.put("study", 1);
+		look.put("survey", 1);
+		look.put("view", 1);
+		look.put("watch", 1);
+		look.put("open", 1); //for the open locker-command
+	}
+
+	private void addUse() {
+		use.put("apply", 1);
+		use.put("give", 1);
+		use.put("insert", 1);
+		use.put("practice", 1);
+		use.put("throw", 1);
+		use.put("unlock", 1);
+		use.put("use", 1);
+		use.put("utilize", 1);
+	}
+
+/*
+@param: a list of tagged words
+@return: A list of better tagged words, where some nouns are now verbs because they should be that.
+
+So, for each word in input, we see if it should be a verb by using the "verbs" method. If it
+returns something, we create a new tagged word with that verb and the tag verb, we remove the old one 
+(there is no replace-function) and then add the new tagged word.
+*/
+	public List<TaggedWord> betterTagging(List<TaggedWord> input) {
+		for(int i = 0; i < input.size(); i++) {
+			TaggedWord wd = input.get(i);
+			String verbifyed = verbs(wd.word());
+			if(verbifyed != null) {
+				TaggedWord res = new TaggedWord(verbifyed, "VB");
+				input.remove(i); //if we don't remove the word, it will get moved one spot.
+				input.add(i, res); //insert the new verb.
+			}
+		}
+		return input;
+	}
+
+	public String verbs(String vb) {
+		if(go.containsKey(vb)) {
+			return "go";
+		} else if(use.containsKey(vb)) {
+			return "use";
+		} else if(take.containsKey(vb)) {
+			return "take";
+		} else if(look.containsKey(vb)) {
+			return "look";
+		} else if(eat.containsKey(vb)) {
+			return "eat";
+		}
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
