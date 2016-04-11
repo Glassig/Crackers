c504a1fd51dc7d2ef60eb87b22b6f85c1e1f1a6b
Kashi <k.klingestedt@gmail.com>
2016-04-12 00:38:50 +0200

    Removed a map

--- game/script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-javac -cp stanford-postagger.jar Item.java Main.java Parser.java Player.java POSTagger.java Room.java Synonyms.java World.java && java -cp ".:stanford-postagger.jar:lib/*" Main
